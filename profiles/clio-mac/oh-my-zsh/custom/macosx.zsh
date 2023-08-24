6c7d63da7ef0f652194cd489d877764f02c5c8c8
Peer Allan <peer.allan@clio.com>
2023-08-24 11:18:49 -0500

    Updates to global and mac confings

--- profiles/clio-mac/oh-my-zsh/custom/macosx.zsh
+++ profiles/clio-mac/oh-my-zsh/custom/macosx.zsh
@@ -1,14 +1,16 @@
-echo "MacOS Setup"
-
 export PATH=/opt/homebrew/bin:/Users/peerallan/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
+
+ZSH_THEME="powerlevel10k/powerlevel10k"
 
 # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
-# if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-#   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-# fi
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 # Preferred editor for local and remote sessions
 if [[ -n $SSH_CONNECTION ]]; then
@@ -21,12 +23,6 @@ export GIT_EDITOR=$EDITOR
 # Compilation flags
 export ARCHFLAGS="-arch arm64"
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
-
-source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /opt/homebrew/share/zsh-history-substring-search/zsh-history-substring-search.zsh
-source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 source "${HOME}/.zsh_secrets"
 
